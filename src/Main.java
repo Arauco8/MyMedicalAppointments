650d7a072b9bcf4ad3771a8e885c939401a6f056
Puma Leiva <apumaleiva@gmail.com>
2023-01-17 22:17:25 -0300

    Creada la Clase Doctor y el pkg UIMENU con el Menu y el Book an appointment

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import static ui.UIMenu.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Declarar el objeto ---- Instanciar el objeto
+        Doctor myDoctor = new Doctor("Alejandro Rodríguez");
+        myDoctor.showName();
+        showMenu();
+    }
+}
